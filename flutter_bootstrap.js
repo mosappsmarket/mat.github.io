5d7a2f0259819e9f3c683988625c1a5435702f9e
mosappsmarket <111380792+mosappsmarket@users.noreply.github.com>
2025-08-01 16:03:18 +0300

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2903975587"
+    serviceWorkerVersion: "4021920335"
   }
 });
